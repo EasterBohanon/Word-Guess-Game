e6c8a5f2e4938a47369008cf1a861efe84ff5659
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-11-13 22:00:17 -0700

    Trying to make it happen

---
+++ assets/javascript/game.js
@@ -0,0 +1,55 @@
+// creating the words to guess
+var wordForce = {
+    choicesForce: {
+        leia: {
+            //something
+        },
+
+        luke: {
+            //something 
+        },
+
+        chewbacca: {
+
+        },
+
+        hanSolo: {
+
+        },
+
+        obiKenobi: {
+
+        },
+
+        darthVader: {
+
+        },
+
+
+    },
+
+
+        
+
+
+};
+
+
+//variables to set up the game, I hope
+
+wordForce: [],
+matchedForceLetters: [],
+guessedForceLetters: [], 
+
+
+
+
+//wordInForce: null,
+lettersOfForce: [],
+matchedForceLetters: [],
+guessedLetters: [],
+guessesLeft: 0,
+totalGuesses: 0,
+letterForceGuessed: null,
+wins: 0,
+
